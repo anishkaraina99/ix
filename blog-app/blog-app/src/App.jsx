db9f5665127fc799240170ec05215c6d1c011fbd
anishkaraina99 <anishka.raina@gmail.com>
2024-05-23 11:01:02 +0200

    blog app initial

---
+++ blog-app/blog-app/src/App.jsx
@@ -0,0 +1,47 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import HomePage from "./components/HomePage";
+
+
+// function App() {
+//   const user = {
+//     firstName: "Byron",
+//     lasttName: "de Villiers",
+//     bio: "bio",
+//     email: "byron@gmail.com",
+//     authenticated: true
+//   };
+
+//   if(user.authenticated)
+//   {
+//     return (<div >
+//       <h1> Welcome to our new app</h1>
+//       <h5> (user.firstName)</h5>
+  
+//      </div>
+//     ) ;
+//   }
+//   else
+//   {
+//     return (<div >
+//     <h1> Please Login</h1>
+//    </div>);
+//   }
+  
+  
+// }
+
+// export default App;
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
