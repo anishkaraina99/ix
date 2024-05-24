7bc2158644a15ddad851d123e78f2fa1aedf2e2c
anishkaraina99 <anishka.raina@gmail.com>
2024-05-24 10:21:34 +0200

    working home page, not functioning blogs page

---
+++ blog-app/frontend/src/components/Blogs/index.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import Navbar from '../ Navbar'
+// import Heading from '../Heading'
+// import CategoryList from '../CategoryList'
+
+// export default function index()
+// {
+//   return (
+//     <>
+//     <Navbar />
+//     <div className="container">
+//       <Heading />
+//       <div className="scroll-menu">
+//         <CategoriesList />
+//       </div>
+//       <div style={{ display: "flex", justifyContent: "space-between" }}>
+//         <p className="page-subtitle">Blog Posts</p>
+//       </div>
+//       <BlogList blogPosts={blogPosts} />
+//     </div>
+//     <Footer />
+//   </>
+//   )
+// }
---
+++ blog-app/frontend/src/components/SubHeading/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+export default function SubHeading({ subHeading }) {
+  return <p className="page-subtitle">{subHeading}</p>;
+}
+
+// SubHeading.prototype = 
+// {
+//   subHeading: PropTypes.string.isRequired
+// }
